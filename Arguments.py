d72daef98640db9ce704278427792554795a5d1c
Andrew Dyl <dylandrej5@gmail.com>
2021-10-27 22:41:41 +0300

    HomeWork #3

---
+++ Arguments.py
@@ -0,0 +1,10 @@
+def text(a, b, c):
+    if a > b:
+        print(c)
+    elif a < b:
+        print("sad text")
+    else:
+        print("funny text")
+text(100, 15, 200)
+text(29,756,200)
+text(567,567,200)
